f03e111f5cc5389039af7205faf95a030da1ed6e
Edward Casbon <edward@edwardcasbon.co.uk>
2014-12-08 12:41:49 +0000

    Added Grunt config

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    /* Sass configuration */
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed',
+          sourcemap: 'none'
+        },
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['*.scss', '*/*.scss'],
+          dest: 'css',
+          ext: '.css'
+        }]
+      }
+    },
+
+    /* JSHint - Check JS syntax */
+    jshint: {
+      options: {},
+      js: ['js/*.js']
+    },
+
+    /* Concat - Concatenate files, without minifying - Useful for debug */
+    concat: {
+      options: {},
+      dist: {
+        src: ['js/*.js', '!js/iskyvideo.min.js'],
+        dest: 'js/iskyvideo.min.js'
+      }
+    },
+
+    /* Uglify - Concatenate and minify JavaScript */
+    uglify: {
+      options: {},
+      js: {
+        files: {
+          'js/iskyvideo.min.js' : ['js/*.js', '!js/iskyvideo.min.js']
+        }
+      }
+    },
+
+    /* Watch scss and js and process when they're updated */
+    watch: {
+      sass: {
+        files: ['css/*.scss', 'css/*/*.scss'],
+        tasks: ['sass']
+      },
+      js: {
+        files: ['js/*.js', '!js/iskyvideo.min.js'],
+        tasks: ['uglify']
+	  }
+	}
+  });
+
+  	// Load task plugins.
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  // Register the default task.
+  grunt.registerTask('default', 'watch');
+};
