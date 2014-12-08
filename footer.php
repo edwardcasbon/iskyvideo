3455848480e7698db8f409a994d6c2ae965b174f
Edward Casbon <edward@edwardcasbon.co.uk>
2014-12-08 16:03:00 +0000

    Added Google Analytics tracking ID

--- footer.php
+++ footer.php
@@ -21,7 +21,7 @@
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-      ga('create', 'UA-XXXX', 'iskyvideo.co.uk');
+      ga('create', 'UA-57448726-1', 'iskyvideo.co.uk');
       ga('send', 'pageview'); 
     </script>
 
